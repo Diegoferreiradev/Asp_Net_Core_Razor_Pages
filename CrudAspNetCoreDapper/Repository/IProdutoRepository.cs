505bc78da98aa950c250d3d99573a9c957ffa1c9
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-02-09 15:27:15 -0300

    Interface e Remoção dos Usings

---
+++ CrudAspNetCoreDapper/Repository/IProdutoRepository.cs
@@ -0,0 +1,14 @@
+﻿using CrudAspNetCoreDapper.Entities;
+using System.Collections.Generic;
+
+namespace CrudAspNetCoreDapper.Repository
+{
+    public interface IProdutoRepository
+    {
+        int Add(Produto produto);
+        List<Produto> GetProdutos();
+        Produto GetProdutoId(int id);
+        int Edit(Produto produto);
+        int Delete(int id);
+    }
+}
